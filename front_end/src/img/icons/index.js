67ddfc11046a341907715b1f479453e20b6b06e2
Erick <ericklima63@gmail.com>
2023-01-04 16:07:31 -0300

    Layout Done

---
+++ front_end/src/img/icons/index.js
@@ -0,0 +1,11 @@
+export { default as ArrowBarRight } from "./ArrowBarRight";
+export { default as ChevronDown } from "./ChevronDown";
+export { default as ChevronUp } from "./ChevronUp";
+export { default as Code } from "./Code";
+export { default as DogPaw } from "./DogPaw";
+export { default as DoorOpenFill } from "./DoorOpenFill";
+export { default as FileCodeFill } from "./FileCodeFill";
+export { default as HouseFill } from "./HouseFill";
+export { default as PeopleFill } from "./PeopleFill";
+export { default as PersonFill } from "./PersonFill";
+export { default as Search } from "./Search";
