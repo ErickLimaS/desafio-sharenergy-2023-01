fbffed1f461761da6337295c87cf938091020ee9
Erick <ericklima63@gmail.com>
2023-01-11 15:04:38 -0300

    Register Customer Page Logic Done

--- front_end/src/pages/Customers/Register/index.tsx
+++ front_end/src/pages/Customers/Register/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Styles from './Register.module.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import ArrowUp from '../../../img/icons/ChevronUp'
 import { registerNewCustomer } from '../../../api/customersMongoServer'
-import AlertMessage from '../../../Components/AlertMessage'
+import Swal from 'sweetalert2'
 
 function Register() {
 
@@ -18,9 +18,9 @@ function Register() {
   const stateInput = React.useRef<HTMLInputElement>(null)
   const countryInput = React.useRef<HTMLInputElement>(null)
 
-  const [serverResponse, setServerResponse] = useState<{ success: boolean, message: string }>()
+  const navigate = useNavigate()
 
-  // verifies if CPF is correct and it really exists
+  // verifies if CPF is correct and exist
   function verifyCpf(cpf: string) {
 
     let some: number = 0;
@@ -44,6 +44,30 @@ function Register() {
 
   }
 
+  // Shows the server response to the user 
+  function showServerResponse(res: { success: boolean, message: string }) {
+
+    if (res.success) {
+      return Swal.fire({
+        title: 'Tudo Certo!',
+        text: `${res.message || 'O cliente foi cadastrado com sucesso.'}`,
+        icon: 'success',
+        confirmButtonColor: 'var(--success)',
+        confirmButtonText: 'Voltar para página Clientes',
+        didClose: () => navigate('/customers')
+      })
+    }
+
+    return Swal.fire({
+      title: 'Um Erro Aconteceu!',
+      text: `${res.message || ''}`,
+      icon: 'error',
+      confirmButtonColor: 'var(--error)',
+      confirmButtonText: 'Tentar Novamente'
+    })
+
+  }
+
   // submits form and sets a response with what server returns
   async function handleForm(e: React.FormEvent) {
 
@@ -51,7 +75,15 @@ function Register() {
 
     if (verifyCpf(cpfInput.current!.value) === false) {
 
-      return alert('cpf')
+      Swal.fire({
+        title: 'CPF não válido.',
+        text: 'O CPF digitado não é valido.',
+        icon: 'warning',
+        confirmButtonColor: 'var(--warning)',
+        confirmButtonText: 'Tentar Novamente'
+      })
+
+      return
 
     }
 
@@ -68,152 +100,158 @@ function Register() {
       cpf: cpfInput.current!.value
     })
 
-    setServerResponse(res)
-
-    return
+    // Shows the server response to the user
+    showServerResponse(res)
 
   }
 
   return (
 
-    <>
-      {serverResponse && (
-        <AlertMessage props={serverResponse} />
-      )}
+    <div className={Styles.container}>
 
-      <div className={Styles.container}>
+      <Link to='/customers'>
+        <ArrowUp style={{ transform: 'rotate(270deg)', marginRight: '8px' }} /> Voltar
+      </Link>
 
-        <Link to='/customers'>
-          <ArrowUp style={{ transform: 'rotate(270deg)', marginRight: '8px' }} /> Voltar
-        </Link>
+      <h1>Cadastrar Cliente</h1>
 
-        <h1>Cadastrar Cliente</h1>
+      <div className={Styles.form_container}>
 
-        <div className={Styles.form_container}>
+        <form onSubmit={(e) => handleForm(e)}>
 
-          <form onSubmit={(e) => handleForm(e)}>
-
-            <div className={`${Styles.flex_row} ${Styles.name_container}`}>
-
-              <div className={Styles.flex_column}>
-                <label>
-                  Primeiro Nome
-                  <input type='text' ref={firstNameInput}
-                    name='first_name'
-                    title='Primeiro Nome'
-                  ></input>
-                </label>
-              </div>
-              <div className={Styles.flex_column}>
-                <label>
-                  Último Nome
-                  <input type='text' ref={lastNameInput}
-                    name='last_name'
-                    title='Último Nome'
-                  ></input>
-                </label>
-              </div>
-
-            </div>
+          <div className={`${Styles.flex_row} ${Styles.name_container}`}>
 
             <div className={Styles.flex_column}>
               <label>
-                Email
-                <input type='email' ref={emailInput}
-                  name='email'
-                  title='Email'
+                Primeiro Nome
+                <input type='text' ref={firstNameInput}
+                  name='first_name'
+                  title='Primeiro Nome'
+                  required
+                ></input>
+              </label>
+            </div>
+            <div className={Styles.flex_column}>
+              <label>
+                Último Nome
+                <input type='text' ref={lastNameInput}
+                  name='last_name'
+                  title='Último Nome'
+                  required
+                ></input>
+              </label>
+            </div>
+
+          </div>
+
+          <div className={Styles.flex_column}>
+            <label>
+              Email
+              <input type='email' ref={emailInput}
+                name='email'
+                title='Email'
+                required
+              ></input>
+            </label>
+          </div>
+
+          <div className={Styles.flex_column}>
+            <label>
+              CPF
+              <input type='text' ref={cpfInput}
+                name='cpf'
+                pattern="[0-9]{11}"
+                title='Ex: 12345678900'
+                required
+              ></input>
+              <small>Apenas números. Nada de traço ou ponto.</small>
+            </label>
+          </div>
+
+          <div className={`${Styles.flex_row} ${Styles.tel_container}`}>
+
+            <div className={Styles.flex_column}>
+              <label>
+                DDD
+                <input type='text' ref={dddTelInput}
+                  name='ddd'
+                  title='DDD do Estado deste Telefone de Contato'
+                  pattern="[0-9]{2}"
+                  required
+                ></input>
+                <small>Ex: 11</small>
+              </label>
+            </div>
+            <div className={Styles.flex_column}>
+              <label>
+                Telefone de Contato
+                <input type='text' ref={telInput}
+                  name='tel'
+                  title='Resto do Telefone de Contato'
+                  pattern="[0-9]{8,9}"
+                  required
+                ></input>
+                <small>Precisa do 9 inicial, dependendo de sua região.</small>
+              </label>
+            </div>
+
+          </div>
+
+          <div className={Styles.flex_column}>
+            <label>
+              Rua onde Mora
+              <input type='text' ref={streetInput}
+                name='street'
+                title='Rua onde Mora'
+                required
+              ></input>
+            </label>
+          </div>
+
+          <div className={Styles.flex_row}>
+
+            <div className={Styles.flex_column}>
+              <label>
+                Bairro
+                <input type='text' ref={countyInput}
+                  name='county'
+                  title='Bairro'
+                  required
                 ></input>
               </label>
             </div>
 
             <div className={Styles.flex_column}>
               <label>
-                CPF
-                <input type='text' ref={cpfInput}
-                  name='cpf'
-                  title='CPF do Cliente'
+                Estado
+                <input type='text' ref={stateInput}
+                  name='state'
+                  title='Estado'
+                  required
                 ></input>
-                <small>Apenas números.</small>
               </label>
-            </div>
-
-            <div className={`${Styles.flex_row} ${Styles.tel_container}`}>
-
-              <div className={Styles.flex_column}>
-                <label>
-                  DDD
-                  <input type='text' ref={dddTelInput}
-                    name='ddd'
-                    title='DDD do Telefone de Contato'
-                  ></input>
-                  <small>Ex: 11.</small>
-                </label>
-              </div>
-              <div className={Styles.flex_column}>
-                <label>
-                  Telefone de Contato
-                  <input type='text' ref={telInput}
-                    name='tel'
-                    title='Telefone de Contato'
-                  ></input>
-                  <small>Apenas números.</small>
-                </label>
-              </div>
-
             </div>
 
             <div className={Styles.flex_column}>
               <label>
-                Rua onde Mora
-                <input type='text' ref={streetInput}
-                  name='street'
-                  title='Rua onde Mora'
+                País
+                <input type='text' ref={countryInput}
+                  name='country'
+                  title='País'
+                  required
                 ></input>
               </label>
             </div>
 
-            <div className={Styles.flex_row}>
+          </div>
 
-              <div className={Styles.flex_column}>
-                <label>
-                  Bairro
-                  <input type='text' ref={countyInput}
-                    name='county'
-                    title='Bairro'
-                  ></input>
-                </label>
-              </div>
+          <button type='submit'>Cadastrar</button>
 
-              <div className={Styles.flex_column}>
-                <label>
-                  Estado
-                  <input type='text' ref={stateInput}
-                    name='state'
-                    title='Estado'
-                  ></input>
-                </label>
-              </div>
-
-              <div className={Styles.flex_column}>
-                <label>
-                  País
-                  <input type='text' ref={countryInput}
-                    name='country'
-                    title='País'
-                  ></input>
-                </label>
-              </div>
-
-            </div>
-
-            <button type='submit'>Cadastrar</button>
-
-          </form>
-
-        </div >
+        </form>
 
       </div >
-    </>
+
+    </div >
   )
 }
 
